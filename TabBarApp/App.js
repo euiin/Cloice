d9ee404ffc6c528c76140a68581032e2ae995259
minizzang <minizzang@kaist.ac.kr>
2021-02-26 16:25:31 +0900

    loading screen

--- TabBarApp/App.js
+++ TabBarApp/App.js
@@ -11,6 +11,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import FirstScreen from './screens/FirstScreen'
 import MainTabScreen from './screens/MainTabScreen';
 import MyProfile from './screens/MyProfile';
 import Setting from './screens/Setting';
@@ -19,24 +20,41 @@ import Login from './login/Login';
 //import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import { DrawerContent} from './screens/DrawerContent';
+import { View } from 'native-base';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
+
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const change = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  change();
+
   return (
-    <NavigationContainer>
-      <Drawer.Navigator drawerContent = {props => <DrawerContent {...props}/>}
-      initialRouteName="Home" // 동영상에서는 없애줌
-      drawerPosition= "right" //drawer가 오른쪽에 있어서 왼쪽으로 잡아당기면 열린다.
-      >
-        <Drawer.Screen name="Home" component={MainTabScreen} />
-        <Drawer.Screen name="MyProfile" component={MyProfile} />
-        <Drawer.Screen name="Setting" component={Setting} />
-        <Drawer.Screen name="Cart" component={Login} />
-      </Drawer.Navigator>
+    isLoading ? (
+      <FirstScreen/>
+    ) :
+    (<>
+        <NavigationContainer>
+        <Drawer.Navigator drawerContent = {props => <DrawerContent {...props}/>}
+        initialRouteName="Home" // 동영상에서는 없애줌
+        drawerPosition= "right" //drawer가 오른쪽에 있어서 왼쪽으로 잡아당기면 열린다.
+        >
+          <Drawer.Screen name="Home" component={MainTabScreen} />
+          <Drawer.Screen name="MyProfile" component={MyProfile} />
+          <Drawer.Screen name="Setting" component={Setting} />
+          <Drawer.Screen name="Cart" component={Login} />
+        </Drawer.Navigator>
 
-
-    </NavigationContainer>
+        </NavigationContainer>
+      </>
+    )
   );
 }
 
